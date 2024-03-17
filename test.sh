2d8ef17ed2414acd99902fde54b45878848100c5
pedresp <pedro.espinosa@alumnos.upm.es>
2024-03-17 18:56:03 +0100

    tests for lab 3.2.15

--- test.sh
+++ test.sh
@@ -9,21 +9,47 @@ check(){
     fi
 }
 
-INPUT=(1 2 3 6 8 20 1000)
-OUTPUT=(1 1 2 3 3 5 44)
-TEST_LENGHT=${#INPUT[@]}
+if [ $# -ne 1 ]; then
+    echo "bad argument" >&2
+    exit 2
+fi
 
 fails=0
 ind=0
-for ((; !$fails && ind<TEST_LENGHT; ind++))
-do
-    echo ${INPUT[ind]} | python pyramid.py | check "Enter the number of blocks: The height of the pyramid: ${OUTPUT[ind]}"
-    fails=$?
-done
+
+case $1 in
+    "pyramid")
+        INPUT=(1 2 3 6 8 20 1000)
+        OUTPUT=(1 1 2 3 3 5 44)
+        TEST_LENGHT=${#INPUT[@]}
+
+        for ((; !$fails && ind<TEST_LENGHT; ind++))
+        do
+            echo ${INPUT[ind]} | python pyramid.py | check "Enter the number of blocks: The height of the pyramid: ${OUTPUT[ind]}"
+            fails=$?
+        done;;
+    
+    "collatz")
+        INPUT=(15 16 1023)
+        TEST_LENGHT=${#INPUT[@]}
+
+        for ((; !fails && ind<TEST_LENGHT; ind++))
+        do
+            echo ${INPUT[ind]} | python collatz.py > file
+            diff file "testing/collatz/test$ind.txt"
+            fails=$?
+        done
+
+        rm file;;
+    *)
+        echo "incorrect test name" >&2
+        exit 3;;
+esac
 
 if [ $fails -eq 1 ]; then
-    echo "test fails at index $((ind-1))" >&2
+    echo "tests fail at index $((ind-1))" >&2
     exit 1
 else
+    echo "tests for $1 ran correctly"
     exit 0
 fi
